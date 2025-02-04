55cf0897498ea023671d0e2d1dd1781af07b9b0b
Payton <paytonsweet@usf.edu>
2025-02-04 15:37:49 -0500

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,12 @@
+// Task 1: Object Properties
+
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "John.doe@gmail.com"
+}; // creates customer object with data
+
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`); // logs each customer property
+
