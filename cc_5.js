d71cf7d7955ceb70a5bc21bd6b34a905b8fe39a1
Payton <paytonsweet@usf.edu>
2025-02-04 15:54:03 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -10,3 +10,17 @@ console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`); // logs each customer property
 
+// Task 2:
+
+let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+ // creates order and properties
+displayOrder(){
+console.log(`OrderId: ${this.orderId}
+Total Amount: $${this.totalAmount}
+Status: ${this.status}`); 
+} // creates a display order to display the properties
+};
+order.displayOrder(); // displays the order in console log
