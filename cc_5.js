0c21b42e9f5364b878761cfb95f2ad64a05ab5c6
Payton <paytonsweet@usf.edu>
2025-02-04 16:03:51 -0500

    Task 3 - Shopping Cart.

--- cc_5.js
+++ cc_5.js
@@ -10,7 +10,7 @@ console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`); // logs each customer property
 
-// Task 2:
+// Task 2: Object Methods
 
 let order = {
     orderId: 12345,
@@ -24,3 +24,12 @@ Status: ${this.status}`);
 } // creates a display order to display the properties
 };
 order.displayOrder(); // displays the order in console log
+
+// Task 3: Array Manipulation
+
+let cartItems = ["Milk", "Eggs", "Bread"]; // creates array of cart items
+cartItems.push("Cookies"); // adds cookies to the end
+cartItems.pop(); // removes item from the end
+cartItems.unshift("Ham"); // adds Ham to the beginning
+cartItems.shift(); // removes item from the beginning
+console.log(`Cart Items: ${cartItems}`); // logs cart items
