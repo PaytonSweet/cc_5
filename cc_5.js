c443599710f9a7a71eeda93f866ed797885634c6
Payton <paytonsweet@usf.edu>
2025-02-06 10:43:53 -0500

    Final Edits Before Submission

--- cc_5.js
+++ cc_5.js
@@ -3,7 +3,7 @@
 let customer = {
     name: "John Doe",
     age: 35,
-    email: "John.doe@gmail.com"
+    email: "john.doe@email.com"
 }; // creates customer object with data
 
 console.log(`Name: ${customer.name}`);
@@ -18,9 +18,7 @@ let order = {
     status: "Processing",
  // creates order and properties
 displayOrder(){
-console.log(`OrderId: ${this.orderId}
-Total Amount: $${this.totalAmount}
-Status: ${this.status}`); 
+console.log(`OrderId: ${this.orderId}, Total Amount: $${this.totalAmount}, Status: ${this.status}`); 
 } // creates a display order to display the properties
 };
 order.displayOrder(); // displays the order in console log
@@ -38,23 +36,23 @@ console.log(`Cart Items: ${cartItems}`); // logs cart items
 
 let prices = [100, 200, 300]; // declared prices variable
 
-function applyDiscount(prices, discountRate){
+function discountApply(prices, discountRate){ // used discount apply name as apply discount is used later on
     return prices.map(price => price * (1 - discountRate)); // mapped discount to each price
 }
-let discountedPrices = applyDiscount(prices, 0.1); // creates new variable representing discount prices
+let discountedPrices = discountApply(prices, 0.1); // creates new variable representing discount prices
 console.log(`Discounted Prices: ${discountedPrices}`); // logs discounted prices
 
 // Task 5: Filter Method
 
 let inventory = [5, 0 , 12, 8, 0]; // declares inventory array
-let stock = inventory.filter(inventory => inventory > 0); // filters out inventory not greater than 1
+let stock = inventory.filter(stock => stock > 0); // filters out inventory not greater than 0
 console.log(`Inventory: ${stock}`); // logs filtered inventory
 
 // Task 6: Reduce Method
 
 let sales = [500, 300, 200, 400]; // creates sales array
 let totalSales = sales.reduce((total, sale) => total + sale, 0); // combines all values in array
-console.log(`Total Sales: $${totalSales}`); // logs total sales
+console.log(`Total Revenue: $${totalSales}`); // logs total sales
 
 // Task 7: find() Method
 
@@ -73,10 +71,10 @@ console.log(`Calculated Tax: $${calculateTax(1000, 0.07)}`); // logs the tax bas
 // Task 9: Function Expression
 
 // made function that calculates discount applied to an item
-function discountApply(price, discount){ // function named discount apply due to apply discount being used in task 4
+function applyDiscount(price, discount){ 
     return price - (price * (discount / 100));
 }
-console.log(`Discounted price: $ ${discountApply(100, 7)}`)
+console.log(`Discounted price: $ ${applyDiscount(100, 7)}`)
 // logs the discounted price
 
 // Task 10: Arrow Function
